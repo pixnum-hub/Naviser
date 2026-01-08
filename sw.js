973fc9f7baf60f139c4c70e8bd5b4d86e486c50f
Manik Roy <pixnum@gmail.com>
2026-01-08 18:21:59 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,31 +1,45 @@
-const CACHE="naviser-v1";
+const CACHE_NAME = "naviser-v1";
 
-const ASSETS=[
- "./",
- "./index.html",
- "./manifest.json",
- "./sw.js",
- "./icons/icon-192.png",
- "./icons/icon-512.png",
- "https://unpkg.com/leaflet/dist/leaflet.css",
- "https://unpkg.com/leaflet/dist/leaflet.js"
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./sw.js",
+  "https://unpkg.com/leaflet/dist/leaflet.css",
+  "https://unpkg.com/leaflet/dist/leaflet.js"
 ];
 
-self.addEventListener("install",e=>{
-e.waitUntil(
-caches.open(CACHE).then(c=>c.addAll(ASSETS))
-);
+/* INSTALL */
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener("activate",e=>{
-e.waitUntil(
-caches.keys().then(keys=>
-Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))
-));
+/* ACTIVATE */
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
-self.addEventListener("fetch",e=>{
-e.respondWith(
-caches.match(e.request).then(r=>r||fetch(e.request))
-);
+/* FETCH */
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => {
+      return res || fetch(event.request);
+    })
+  );
 });
