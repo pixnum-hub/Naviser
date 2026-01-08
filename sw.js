c153efc9ab33f065853f4b8b752b2624cf3f4c8b
Manik Roy <pixnum@gmail.com>
2026-01-08 17:22:50 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,12 @@
-const CACHE_NAME = 'naviser-cache-v1';
+const CACHE_NAME = "naviser-cache-v1";
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://unpkg.com/leaflet/dist/leaflet.css',
-  'https://unpkg.com/leaflet/dist/leaflet.js',
-  'https://unpkg.com/leaflet-geometryutil'
+  "/index.html","/manifest.json","/sw.js",
+  "https://unpkg.com/leaflet/dist/leaflet.css",
+  "https://unpkg.com/leaflet/dist/leaflet.js",
+  "https://unpkg.com/leaflet-geometryutil",
+  "/icons/icon-192.png","/icons/icon-512.png"
 ];
 
-// Install SW and cache assets
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-// Activate SW
-self.addEventListener('activate', event => {
-  event.waitUntil(clients.claim());
-});
-
-// Fetch from cache first, then network
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
-});
+self.addEventListener("install", e => e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache))));
+self.addEventListener("activate", e => e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))));
+self.addEventListener("fetch", e => e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
