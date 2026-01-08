4067402c6bd8763a665f4ba94816d525b7b5855f
Manik Roy <pixnum@gmail.com>
2026-01-08 16:41:10 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,44 @@
-const CACHE_NAME='naviser-final-v2';
-const OFFLINE_URLS=[
-  './','./index.html','./manifest.json','./icon-192.png','./icon-512.png',
+const CACHE_NAME = 'naviser-cache-v1';
+const APP_SHELL = [
+  '/',
+  'index.html',
+  'manifest.json',
+  'sw.js',
+  'icons/icon-192.png',
+  'icons/icon-512.png',
   'https://unpkg.com/leaflet/dist/leaflet.css',
-  'https://unpkg.com/leaflet/dist/leaflet.js'
-];
-const TILE_SERVERS=[
- 'https://a.tile.openstreetmap.org/','https://b.tile.openstreetmap.org/','https://c.tile.openstreetmap.org/',
- 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/',
- 'https://a.tile.opentopomap.org/','https://b.tile.opentopomap.org/','https://c.tile.opentopomap.org/',
- 'https://a.basemaps.cartocdn.com/dark_all/','https://a.basemaps.cartocdn.com/light_all/'
+  'https://unpkg.com/leaflet/dist/leaflet.js',
+  'https://unpkg.com/leaflet-geometryutil',
+  'https://cdn.jsdelivr.net/npm/idb@7/build/iife/index-min.js'
 ];
 
-self.addEventListener('install',e=>{
- e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(OFFLINE_URLS)));
- self.skipWaiting();
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch',e=>{
- const url=e.request.url;
- if(TILE_SERVERS.some(t=>url.startsWith(t))){
-  e.respondWith(
-   caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{ caches.open(CACHE_NAME).then(c=>c.put(e.request,resp.clone())); return resp; }).catch(()=>caches.match('./')))
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => { if(key !== CACHE_NAME) return caches.delete(key); })
+      )
+    )
   );
-  return;
- }
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).then(fetchRes => {
+        if(event.request.url.startsWith('http')){
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, fetchRes.clone()));
+        }
+        return fetchRes;
+      }).catch(()=>{});
+    })
+  );
 });
