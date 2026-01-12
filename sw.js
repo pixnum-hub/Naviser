287f8365f4097deafbf95f09b84314e9e0a666a3
Manik Roy <pixnum@gmail.com>
2026-01-12 16:56:49 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE = "naviser-v4";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
-  "./icons/icon-maskable-192.png",
-  "./icons/icon-maskable-512.png"
+const CACHE="naviser-v5";
+const ASSETS=[
+ "./Naviser/",
+ "./Naviser/index.html",
+ "./Naviser/manifest.json",
+ "./Naviser/icons/icon-192.png",
+ "./Naviser/icons/icon-512.png",
+ "./Naviser/icons/icon-maskable-192.png",
+ "./Naviser/icons/icon-maskable-512.png"
 ];
 
 self.addEventListener("install",e=>{
@@ -16,14 +16,12 @@ self.addEventListener("install",e=>{
 
 self.addEventListener("activate",e=>{
  e.waitUntil(
-  caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k))))
+   caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k))))
  );
  self.clients.claim();
 });
 
 self.addEventListener("fetch",e=>{
  if(e.request.method!=="GET") return;
- e.respondWith(
-  caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match("./index.html")))
- );
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match("./Naviser/index.html"))));
 });
